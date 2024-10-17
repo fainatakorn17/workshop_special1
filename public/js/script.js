23fd7fab4fc4fe8bb9535e94bea0248f62cb9f3a
Natakorn Lohasuwan <natakorn.loh@dome.tu.ac.th>
2024-10-18 02:52:01 +0700

    second commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,17 @@
 function submitLogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-
     fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
-            'Application-Key' : 'TU8707e41c55cefd2a00b7d120ca912edb9952810872f990a5bd0ceac7480aab6c83b532d66c546bb3e86c3c0bd7187f1e'
+            'Application-Key' : 'TU8707e41c55cefd2a00b7d120ca912edb9952810872f990a5bd0ceac7480aab6c83b532d66c546bb3e86c3c0bd7187f1e',
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ "UserName ":username,"PassWord" : password })
+        body: JSON.stringify({ "UserName":username,"PassWord" : password })
     })
     .then(response => response.json())
     .then(data => {
         document.getElementById('message').innerText = data.message;
-    })
-    .catch(error => console.error('Error:', error));
-}
-
-
-
-function call_REST_API_Hello() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    const url = (
-        'http://localhost:8080/hello?' +
-        new URLSearchParams({ myName: username, lastName: password}).toString()
-      );
-    
-    fetch(url)
-    .then(response => response.text())
-    .then(text => {
-        document.getElementById('message').innerText = text;
     })
     .catch(error => console.error('Error:', error));
 }
